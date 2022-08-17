85a9520742ba42b045415b30ad630237e63bafb1
Danheon <eksgjs3847@gmail.com>
2022-08-17 18:30:47 +0900

    📦 React-Query 적용 및 suspense 에러 처리

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorPage from "./components/common/ErrorPage";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={true} />
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <App />
+      </Suspense>
+    </ErrorBoundary>
+  </QueryClientProvider>,
+);
